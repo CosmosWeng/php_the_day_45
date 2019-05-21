830438e6f53daaf3f82b3578b71bbd697f09e3ca
Cosmos <cosmos-s@hotmail.com.tw>
2019-05-21 20:40:17 +0800

    📚 Docs: First push

---
+++ src/Health.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PhpTheDay;
+
+class Health
+{
+    public function bmi(int $h, int $w)
+    {
+        return $w / pow($h * 100.0, 2);
+    }
+}
---
+++ tests/HealthTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use PhpTheDay\Health;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * HealthTest
+ * @group health
+ */
+class HealthTest extends TestCase
+{
+    /** @test */
+    public function test_bmi()
+    {
+        // Test
+        $target   = new Health();
+        $expected = 22.0;
+        $bmi      = $target->bmi(165, 60);
+        $this->assertEquals($expected, $bmi);
+    }
+}
